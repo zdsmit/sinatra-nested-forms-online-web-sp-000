aa45fff4af714aff69d36f0c2cf8d82c28e5ed98
Zach  Smith <zdsmith001@gmail.com>
2019-11-21 17:50:44 +0000

    Done.

--- app.rb
+++ app.rb
@@ -4,6 +4,16 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+      erb ':pirates/new'
+    end
+
+    post '/pirate' do
+    end
 
   end
 end
